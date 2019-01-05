c0e970214aacc757c5242c0014290d57700cba17
Aidan Holland <thehappydinoa@gmail.com>
2019-01-05 15:47:32 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,42 @@
-from os import getenv
+import os
+from io import BytesIO
+from time import sleep
+from uuid import uuid4
+from zipfile import BadZipfile, ZipFile
 
-from boto3 import client
+import boto3
 
 from sanic import Sanic, response
 
 app = Sanic()
 
-s3 = client("s3")
+s3 = boto3.resource("s3")
+bucket = s3.Bucket("pix3lify-logs")
 
-REDIRECT_URL = getenv("REDIRECT_URL", "https://github.com/Magisk-Modules-Repo/Pix3lify")
+
+REDIRECT_URL = os.getenv(
+    "REDIRECT_URL", "https://github.com/Magisk-Modules-Repo/Pix3lify")
+
+
+class BadExtensionException(Exception):
+    pass
+
+
+async def save_logs(zip_content, folder):
+    zip_file = "logs/" + folder + ".zip"
+    try:
+        open(zip_file, "wb").write(zip_content)
+        zip = ZipFile(zip_file, "r")
+        for filename in zip.namelist():
+            if any(ext in filename for ext in [".log", ".txt", ".xml", ".prop"]) and not any(ext in filename for ext in ["__MACOSX"]):
+                print("Uploading " + filename)
+                bucket.upload_fileobj(BytesIO(zip.read(
+                    filename)), "/".join([folder, filename]), ExtraArgs={'ACL': 'public-read'})
+    except BadZipfile:
+        print("Bad Zipfile")
+    finally:
+        os.remove(zip_file)
+
 
 @app.route("/")
 def index(request):
@@ -16,10 +44,17 @@ def index(request):
 
 
 @app.route("/logs", methods=["POST"])
-def submit_logs(request):
-    print(request.headers)
-    print(request.body)
-    return response.text("OK")
+async def submit_logs(request):
+    logs_file = request.files.get("logs")
+    print(logs_file.type)
+    key = str(uuid4())
+    if len(zip_content) < 500000:
+        app.add_task(save_logs(logs_file.body, key))
+        return response.json({
+            "key": key,
+        })
+    return response.text("Failed zip file too large")
 
-
-app.run(host="0.0.0.0", port=int(getenv("PORT", 5000)), workers=2)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)),
+            workers=int(os.getenv("WORKERS", 1)), auto_reload=True)
